a53641dce675745e79b32722dd2d659c201e7b70
yunhee.kim <kyh901126@nate.com>
2021-05-02 13:24:40 +0900

    [서블릿] HttpServletResponse - 기본 사용법

---
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,66 @@
+package hello.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * http://localhost:8080/response-header
+ */
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //[status-line]
+        response.setStatus(HttpServletResponse.SC_OK); // 매직넘버 200 대신 의미있는 값으로 쓰자!
+
+        //[response-headers]
+        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        // 캐시를 아예 무효화하겠다 정의
+        response.setHeader("Cache-Control", "no-cache, no-store, must- revalidate");
+        response.setHeader("Pragma", "no-cache"); // 과거 버전까지 캐시 무효화해주는 부분
+        response.setHeader("my-header", "hello");
+
+        //[Header 편의 메서드]
+        content(response);
+        cookie(response);
+        redirect(response);   // 여기 활성화 시 리다이렉트 된다.
+
+        //[message body]
+        PrintWriter writer = response.getWriter();
+        writer.println("ok");
+    }
+
+    private void content(HttpServletResponse response) {
+        //Content-Type: text/plain;charset=utf-8
+        //Content-Length: 2
+        // 이 헤더 부분은 아래의 2줄로 나눠 나타낼 수 있다.
+        //response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        //response.setContentLength(2); //(생략시 자동 생성)
+    }
+
+    private void cookie(HttpServletResponse response) {
+        // 이 쿠키 부분은 아래와 같이 나타낼 수 있다.
+        //Set-Cookie: myCookie=good; Max-Age=600; //response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600); //600초
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) throws IOException {
+        //Status Code 302
+        //Location: /basic/hello-form.html
+
+        // 이 redirect 하는 부분은 이렇게 한줄로 표현할 수 있다.
+        //response.setStatus(HttpServletResponse.SC_FOUND); //302
+        //response.setHeader("Location", "/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html");
+    }
+}
