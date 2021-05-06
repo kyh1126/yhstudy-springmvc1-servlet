8e6158abe0defeab9d430e3de3ed00cde41bd135
yunhee.kim <kyh901126@nate.com>
2021-05-06 17:25:00 +0900

    [스프링 MVC - 구조 이해] 뷰 리졸버

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,8 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 // 서블릿 자동 등록
 // 스프링 부트는 서블릿을 직접 등록해서 사용할 수 있도록 @ServletComponentScan 을 지원한다.
@@ -13,4 +15,14 @@ public class ServletApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServletApplication.class, args);
     }
+
+    /**
+     * 이런 bean 부분을 스프링 부트는 application.properties 값을 보고 서블릿 자동 등록한다.<p>
+     * * spring.mvc.view.prefix=/WEB-INF/views/<p>
+     * * spring.mvc.view.suffix=.jsp
+     */
+    /*@Bean
+    InternalResourceViewResolver internalResourceViewResolver() {
+        return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+    }*/
 }
